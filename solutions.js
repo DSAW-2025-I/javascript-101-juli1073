303662dee56f1475311b34f24de2693e54788b7f
juli1073 <julianagonzalezpolania@gmail.com>
2025-02-21 19:16:25 -0500

    Completar el codigo

--- solutions.js
+++ solutions.js
@@ -1,26 +1,28 @@
 // Sum of Two Numbers
-function sum(a, b) {
+function sum(b,a ) {
   // Add your code here
+  sum(1,2);
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  factorial(5);
 }
 
 // Find the Largest Number
 function findLargest(arr) {
   // Add your code here
+  findLargest(2,3,4,5);
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  countVowels("juliana");
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  isPrime(24);
 }
 
 module.exports = {
